89691a8ba4ee0e25f29bd6b2b89af282a66690fa
Skrilliks47 <borisovsasha16072004@gmail.com>
2026-04-01 18:06:29 +0300

    add line: additional message

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,17 +1,13 @@
 ﻿namespace Program;
+namespace Program;
 
 class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
-        Console.WriteLine("Hello, World!");
-=======
-        Console.WriteLine("Alexandr");
+        Console.WriteLine("Enter your name:");
         string? name = Console.ReadLine();
-        Console.WriteLine($"Hello, {name}");
-        Console.WriteLine("How are you?");
-         Console.WriteLine("Это новое изменение для коммита!");
->>>>>>> 1ff6867630d7e24cdc3896176e2c619c08328a86
+        Console.WriteLine($"Hello, {name}!");
+         Console.WriteLine("How are you?");
     }
 }
